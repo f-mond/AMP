cdb170e9885dade9f837d501ec412d7fd7d430cd
f-mond <ferdinandmond@gmail.com>
2022-06-16 23:31:04 +0200

    added throughput test but RDCSS Complete causes seg faults

--- test.cpp
+++ test.cpp
@@ -7,12 +7,14 @@
 #include <random>
 #include <algorithm>
 #include <chrono>
+//#include "casn.cpp"
 
 #define UNDECIDED 0
 #define SUCCEEDED 1
 #define FAILED 2
 #define CASNDESC 3
 #define RDCSSDESC 2
+
 
 class Zeugs {
     public:
@@ -108,13 +110,6 @@ void* SetTagRDCSS(void *p1) {
     return p2;
 }
 
-/*
-RDCSSDescriptor GetDescriptorRDCSS(void *p2) {
-    const uintptr_t MASK = ~0x03ULL;
-    intptr_t p3 = (uintptr_t)p2 & MASK;
-    return *(RDCSSDescriptor*)p3;
-}
-*/
 
 CASNDescriptor* GetDescriptorCASN(void *p2) {
     const uintptr_t MASK = ~0x03ULL;
@@ -123,9 +118,11 @@ CASNDescriptor* GetDescriptorCASN(void *p2) {
 }
 
 long int CAS1(std::atomic<long int> &a, long int o, long int n) {
+	long int tmp = o;
     long int old=a;
     //std::cout << "doing CAS on value " << a << " with expected value " << o << " and new value " << n << std::endl;
     a.compare_exchange_weak(o, n);
+    o=tmp;
     //std::cout << "CAS resulted in " << a << std::endl;
     return old;
 }
@@ -138,6 +135,8 @@ void Complete(RDCSSDescriptor *d) {
 	if(v==d->o1) {
 	//std::cout << "attempting second CAS1 with a2 " << *d->a2 << " expected value " << (long int)SetTag(d) << " new value " << d->n2 << std::endl;
 	//std::cout << "CAS1" << std::endl;
+	//std::cout << IsDescriptor((void*)d) << std::endl;
+	//std::cout << (long int)d << std::endl;
 	CAS1(*d->a2,(long int)SetTagRDCSS(d),d->n2);
 	//std::cout << "second CAS1 finished with a2 now " << *d->a2 << std::endl;
 	}
@@ -155,8 +154,8 @@ long int RDCSS (RDCSSDescriptor *d, int id) {
         r=CAS1(*d->a2, d->o2, (long int)SetTagRDCSS(d));
         //std::cout << "thread " << id << " first CAS happened, value " << r << " returned, we now have value " << *d->a2 << " at the data location" << std::endl;
         if(IsDescriptor((void*)r)==RDCSSDESC && r>INT_MAX) {
-        std::cout << "thread " << id << " we found a descriptor in RDCSS " << r << " and will complete it now " << r << std::endl;
-        Complete((RDCSSDescriptor*)GetDescriptorCASN((void*)r));
+        std::cout << "thread " << id << " we found a descriptor in RDCSS " << (void*)r << " and will complete it now " << IsDescriptor((void*)r) << std::endl;
+        Complete((RDCSSDescriptor*)GetDescriptor((void*)r));
         }
         //std::cout << "r " << r << std::endl;
     } while(IsDescriptor((void*)r)==RDCSSDESC && r>INT_MAX);
@@ -177,20 +176,22 @@ bool CASN(CASNDescriptor *cd,int id =0){
 			//std::cout << "thread " << id << " starting loop with i " << i << " count " << cd->count << " status " << status << std::endl;
 			retry_entry: auto entry = cd->entry[i];
 			RDCSSDescriptor temp = RDCSSDescriptor(&cd->status, entry.addr, UNDECIDED, entry.oldVal, (long int)SetTagCASN(cd));
+			//std::cout << "created RDCSS Descriptor " << (void*)&temp << std::endl;
 			long int val = RDCSS(&temp,id);
 			//std::cout << "thread " << id << " value returned from RDCSS on entry " << i << " : " << val << " and address set to " << *entry.addr << std::endl;
 			if(IsDescriptor((void*)val)==CASNDESC && val> INT_MAX){
-				std::cout << "found CASNDescriptor " << val << std::endl;
+				//std::cout << "found CASNDescriptor " << val << std::endl;
 				if(cd != GetDescriptorCASN((void*)val)){
-					std::cout << "thread " << id << " found a descriptor and doing CASN on that one now " << val << std::endl << std::endl;
+					std::cout << "thread " << id << " found a descriptor and doing CASN on that one now " << (void*)val << std::endl << std::endl;
 					CASN(GetDescriptorCASN((void*)val));
+					std::cout << "reentering" << std::endl;
 					goto retry_entry;
-					std::cout << "going to reentry" << std::endl;
+					//std::cout << "going to reentry" << std::endl;
 				}
 			}
 			else if(val != entry.oldVal){
 				status = FAILED;
-				std::cout << "failed to assign values, got value " << val << " instead of value " << entry.oldVal << std::endl;
+				//std::cout << "failed to assign values, got value " << val << " instead of value " << entry.oldVal << std::endl;
 			}
 			//std::cout << "thread " << id << " finished entry " << i << " of " << cd->count << ", having written value " << *cd->entry[i].addr << " to the entry" << std::endl;
 		}
@@ -204,7 +205,7 @@ bool CASN(CASNDescriptor *cd,int id =0){
 	}
 	for(int i=0; i<cd->count; i++){
 		CAS1(*(cd->entry[i].addr), (long int)SetTagCASN(cd), succeeded?(cd->entry[i].newVal) : (cd->entry[i].oldVal));
-		//std::cout << "thread " << id << " writing value " << (succeeded?(cd->entry[i].newVal) : (cd->entry[i].oldVal)) << " to entry " << i << std::endl;
+		//if(id==1) std::cout << "thread " << id << " writing value " << (succeeded?(cd->entry[i].newVal) : (cd->entry[i].oldVal)) << " to entry " << i << std::endl;
 	}
 	return succeeded;
 }
@@ -243,67 +244,117 @@ int main() {
     */
     std::atomic<long int> ato{3},ato2{3},ato3{3}, ato4{3}, ato5{3}, ato6{3}, ato7{3}, ato8{3};
 	entries entry{&ato, 3, 5}, entry2{&ato2,3,5}, entry3{&ato3,3,5}, entry4{&ato4,3,5}, entry5{&ato5,3,5}, entry6{&ato6,3,5}, entry7{&ato7,3,4}, entry8{&ato8,3,3};
+	entries entry2_0{&ato, 5,3}, entry2_2{&ato2, 5,3}, entry2_3{&ato3, 5,3}, entry2_4{&ato4, 5,3}, entry2_5{&ato5, 5,3}, entry2_6{&ato6, 5,3}, entry2_7{&ato7, 4,3}, entry2_8{&ato8, 3,3};
     omp_set_dynamic(0);
     omp_set_num_threads(2);
     std::vector<int> casn_runtime;
     std::vector<int> cas_runtime;
+    std::vector<int> casn_throughput;
+    std::vector<int> cas_throughput;
 	#pragma omp parallel shared(ato)
 	{
 		int id = omp_get_thread_num();
 	    
 	    std::atomic<long int> ato9{4}, ato10{4};
 	    entries entry9{&ato9,4,5}, entry10{&ato10,4,5};
+	    entries entry2_9{&ato9, 5,4}, entry2_10{&ato10,5,4};
 	    std::vector<entries> ent;
+	    std::vector<entries> ent2;
 	    std::vector<int> old_values;
+	    std::vector<bool> shared;
 	    if(id==0){
 	    ent.push_back(entry);
+	    ent2.push_back(entry2_0);
 	    old_values.push_back(*entry.addr);
+	    shared.push_back(false);
 	    ent.push_back(entry2);
+	    ent2.push_back(entry2_2);
 	    old_values.push_back(*entry2.addr);
+	    shared.push_back(false);
 	    ent.push_back(entry3);
+	    ent2.push_back(entry2_3);
 	    old_values.push_back(*entry3.addr);
+	    shared.push_back(false);
 	    }
 	    if(id==1){
 	    ent.push_back(entry4);
+	    ent2.push_back(entry2_4);
+	    shared.push_back(false);
 	    old_values.push_back(*entry4.addr);
 	    ent.push_back(entry5);
+	    ent2.push_back(entry2_5);
+	    shared.push_back(false);
 	    old_values.push_back(*entry5.addr);
 	    ent.push_back(entry6);
+	    ent2.push_back(entry2_6);
+	    shared.push_back(false);
 	    old_values.push_back(*entry6.addr);
 	    }
 	    ent.push_back(entry7);
+	    ent2.push_back(entry2_7);
+	    shared.push_back(true);
 	    old_values.push_back(*entry7.addr);
 	    ent.push_back(entry8);
+	    ent2.push_back(entry2_8);
+	    shared.push_back(true);
 	    old_values.push_back(*entry8.addr);
 	    ent.push_back(entry9);
+	    ent2.push_back(entry2_9);
+	    shared.push_back(true);
 	    old_values.push_back(*entry9.addr);
 	    ent.push_back(entry10);
+	    ent2.push_back(entry2_10);
+	    shared.push_back(true);
 	    old_values.push_back(*entry10.addr);
 	    
         CASNDescriptor casndesc(ent);
-		std::cout << "starting CASN on thread " << id << " size " << casndesc.count << std::endl;
+        CASNDescriptor casndesc2(ent2);
+		//std::cout << "starting CASN on thread " << id << " size " << casndesc.count << std::endl;
 		auto start = std::chrono::high_resolution_clock::now();
-		bool ran = CASN(&casndesc,id);
+		bool ran, ran2;
+		for(int i=0;i<10;i++){
+		ran = CASN(&casndesc,id);
+		casndesc.status = UNDECIDED;
+		ran2 = CASN(&casndesc2,id);
+		casndesc2.status = UNDECIDED;
+		}
 		auto stop = std::chrono::high_resolution_clock::now();
 		auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop-start);
 		std::cout << "CASN resulted in " << (ran?("succeeded") : ("failed")) << " on thread " << id << std::endl;
 		casn_runtime.push_back(duration.count());
+		//std::cout << "ran " << ran << std::endl;
 		for(int i=0;i<old_values.size();i++){
 			//checking if the values were reset in case of fail, or if they got changed in case of success
-			if((ran && *ent[i].addr!=ent[i].newVal) || (!ran && *ent[i].addr != old_values[i])) std::cout << "check failed" << std::endl;
+			//if((ran2 && *ent2[i].addr!=ent2[i].newVal) || (!ran && *ent2[i].addr != ent[i].newVal) && shared[i]==false) std::cout << "check failed thread " << id << " on entry " << i << " should be " << (ran? ent[i].newVal : ent[i].newVal) << " but is " << (*ent[i].addr) << std::endl;
 		}
 		start = std::chrono::high_resolution_clock::now();
 		long int tmp = 3;
-		for(int i=0;i<old_values.size();i++){
+		for(int i=0;i<old_values.size()*20;i++){
 			ato.compare_exchange_weak(tmp,(long int)3);
 		}
 		stop = std::chrono::high_resolution_clock::now();
 		auto duration_base = std::chrono::duration_cast<std::chrono::nanoseconds>(stop-start);
 		cas_runtime.push_back(duration_base.count());
+		
+		int throughput=0;
+		start = std::chrono::high_resolution_clock::now();
+		while(std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::high_resolution_clock::now() - start).count() < 3000) {
+		ran = CASN(&casndesc,id);
+		casndesc.status = UNDECIDED;
+		ran2 = CASN(&casndesc2,id);
+		casndesc2.status = UNDECIDED;
+		throughput+=1;
+		throughput+=1;
+		std::cout << throughput << std::endl;
+		}
+		casn_throughput.push_back(throughput);
 	}
 	for(int i=0;i<cas_runtime.size();i++){
-		std::cout << "thread " << i << " runtime for CASN " << casn_runtime[i] << " runtime for N CAS " << cas_runtime[i] << " ratio " << (double)casn_runtime[i]/cas_runtime[i] << std::endl;
+		std::cout << "thread " << i << " runtime for 20 CASN of size 7: " << casn_runtime[i] << " runtime for 20*7 CAS " << cas_runtime[i] << " ratio " << (double)casn_runtime[i]/cas_runtime[i] << std::endl;
 		}
+	for(int i=0;i<casn_throughput.size();i++){
+		std::cout << "throughput casn " << (int)casn_throughput[i] << std::endl;
+	}
 
 	std::cout << "finished execution" << std::endl;
     
